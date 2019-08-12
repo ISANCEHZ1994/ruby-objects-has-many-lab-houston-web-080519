10e5d74c755bd32de2ad88ad5262583cd9d3415c
Israel Sanchez <israel.pro1994@gmail.com>
2019-08-12 20:12:09 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -7,17 +7,13 @@ class Post
   def initialize (title)
     @title = title 
     @@all << self
-    @author = nil
+    
   end
   
   def self.all
     @@all 
   end 
   
-  def author
-     if @author != nil 
-       then @author = author
-    end
-  end
+ 
   
 end 
