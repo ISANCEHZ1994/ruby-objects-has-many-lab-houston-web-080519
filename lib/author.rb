6da1d725f8820c619f371aec6fda8ec8bcb320a3
Israel Sanchez <israel.pro1994@gmail.com>
2019-08-12 20:52:04 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,8 +17,17 @@ class Author
     Post.all.select {|post|  post.author == self} 
   end 
  
- def add_post(title)
+ def add_post_by_title(title)
+   post = Post.new(title)
+   post.author = self 
+ end 
+ 
+ def self.post_count
+   Post.all.count
+ end 
    
-
+  def self.all 
+    @@all 
+  end 
   
 end 
